26402d1aa1af297bdddf53815a742d50001b3a20
KDS-study <51149024+KDS-study@users.noreply.github.com>
2019-09-10 17:52:32 +0900

    Update moveit_interface_example.cpp

--- src/moveit_interface_example.cpp
+++ src/moveit_interface_example.cpp
@@ -1,31 +1,179 @@
 #include <ros/ros.h>
 #include <moveit_interface_example/MoveitPlanner.h>
+#include  <stdio.h>
 
 using namespace moveit::planning_interface;
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
-    ros::init(argc, argv, "moveit_interface_example");
-    ros::NodeHandle node;
+	double J1 = -2.617994;
+	double J2 = -1.047198;
+	double J3 = 0.3141593;
+	double J4 = -2.96706;
+	double J5 = -1.658063;
+	double J6 = -2.96706;
+	double speed = 0.1;
 
-    //moveitの動作計画を使用するために必要
-    ros::AsyncSpinner spinner(2);
-    spinner.start();
+	char key;
 
-    MoveitPlanner moveitPlanner("move_group");
+	ros::init(argc, argv, "moveit_interface_example");
+	ros::NodeHandle node;
 
-    const std::vector<double> jointRadians = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
-    const auto moveResult = moveitPlanner.moveByJointValues(jointRadians);
+	//moveitの動作計画を使用するために必要
+	ros::AsyncSpinner spinner(2);
+	spinner.start();
 
-    if (moveResult == MoveItErrorCode::SUCCESS)
-    {
-        ROS_INFO("Succeeded to move cobotta");
-    }
-    else
-    {
-        ROS_WARN_STREAM("Failed to move cobotta. error code: " << moveResult);
-    }
+	MoveitPlanner moveitPlanner("arm");
+	while (true)
+	{
+		key = getchar();
+		switch (key) {
+		case '1':
+			speed *= 0.1;
+			ROS_INFO("Speed * 0.1");
+			break;
+		case '2':
+			speed = 0.1;
+			ROS_INFO("Speed initialized");
+			break;
+		case '3':
+			speed *= 10;
+			ROS_INFO("Speed * 10");
+			break;
 
-    ros::waitForShutdown();
-    return 0;
+		case 'a':
+			J1 += speed;
+			break;
+		case 'z':
+			J1 -= speed;
+			break;
+
+		case 's':
+			J2 += speed;
+			break;
+		case 'x':
+			J2 -= speed;
+			break;
+
+		case 'd':
+			J3 += speed;
+			break;
+		case 'c':
+			J3 -= speed;
+			break;
+
+		case 'f':
+			J4 += speed;
+			break;
+		case 'v':
+			J4 -= speed;
+			break;
+
+		case 'g':
+			J5 += speed;
+			break;
+		case 'b':
+			J5 -= speed;
+			break;
+
+		case 'h':
+			J6 += speed;
+			break;
+		case 'n':
+			J6 -= speed;
+			break;
+
+		default:
+			ROS_INFO("Push 1,2,3 azsxdcfvgbhn");
+			continue;
+		}
+
+		if (J1 < -2.617994)
+		{
+			J1 = -2.617994;
+			ROS_INFO("J1 lower");
+		}
+
+		if (J1 > 2.617994)
+		{
+			J1 = 2.617994;
+			ROS_INFO("J1 upper");
+		}
+
+		if (J2 < -1.047198)
+		{
+			J2 = -1.047198;
+			ROS_INFO("J2 lower");
+		}
+
+		if (J2 > 1.743461)
+		{
+			J2 = 1.743461;
+			ROS_INFO("J2 upper");
+		}
+
+		if (J3 < 0.3141593)
+		{
+			J3 = 0.3141593;
+			ROS_INFO("J3 lower");
+		}
+
+		if (J3 > 2.443461)
+		{
+			J3 = 2.443461;
+			ROS_INFO("J3 upper");
+		}
+
+		if (J4 < -2.96706)
+		{
+			J4 = -2.96706;
+			ROS_INFO("J4 lower");
+		}
+
+		if (J4 > 2.96706)
+		{
+			J4 = 2.96706;
+			ROS_INFO("J4 upper");
+		}
+
+		if (J5 < -1.658063)
+		{
+			J5 = -1.658063;
+			ROS_INFO("J5 lower");
+		}
+
+			if (J5 > 2.356194)
+			{
+				J5 = 2.356194;
+				ROS_INFO("J5 upper");
+			}
+
+			if (J6 < -2.96706)
+			{
+				J6 = -2.96706;
+				ROS_INFO("J6 lower");
+			}
+
+			if (J6 > 2.96706)
+			{
+				J6 = 2.96706;
+				ROS_INFO("J6 upper");
+			}
+
+			const std::vector<double> jointRadians = { J1,J2,J3,J4,J5,J6 };
+			const auto moveResult = moveitPlanner.moveByJointValues(jointRadians);
+
+			if (moveResult == MoveItErrorCode::SUCCESS)
+			{
+				ROS_INFO("Succeeded to move cobotta");
+			}
+			else
+			{
+				ROS_WARN_STREAM("Failed to move cobotta. error code: " << moveResult);
+			}
+		}
+
+		ros::waitForShutdown();
+		return 0;
+	
 }
