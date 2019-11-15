14daee1b5d800f495b37bef1b01b897bf8f34543
DONGSIK KIM <51149024+KDS-study@users.noreply.github.com>
2019-11-15 15:59:47 +0900

    Update moveit_interface_example.cpp

--- src/moveit_interface_example.cpp
+++ src/moveit_interface_example.cpp
@@ -87,7 +87,6 @@ void error(char* msg)
 
 int main(int argc, char** argv)
 {
-	int sock0;
 	struct sockaddr_in addr;
 	struct sockaddr_in client;
 	int len;
@@ -203,7 +202,7 @@ int main(int argc, char** argv)
 		string JJ = "J" + to_string(J1);
 		
 		/* ソケットの作成 */
-		sock0 = socket(AF_INET, SOCK_STREAM, 0);
+		sock = socket(AF_INET, SOCK_STREAM, 0);
 
 
 		if (sock < 0) {
@@ -216,29 +215,24 @@ int main(int argc, char** argv)
 		addr.sin_port = htons(12345);
 		addr.sin_addr.s_addr = INADDR_ANY;
 
-		bind(sock0, (struct sockaddr*) &addr, sizeof(addr));
+		bind(sock, (struct sockaddr*) &addr, sizeof(addr));
 
 		/* TCPクライアントからの接続要求を待てる状態にする */
-		listen(sock0, 5);
+		listen(sock, 5);
 		
 
 		/* TCPクライアントからの接続要求を受け付ける */
 		len = sizeof(client);
-		sock = accept(sock0, (struct sockaddr *) &client, (socklen_t*)&len);
+		sock = accept(sock, (struct sockaddr *) &client, (socklen_t*)&len);
 
 		/* 送信 */
-		sendto(sock, JJ, sizeof(JJ),0, (struct sockaddr*)& addr, sizeof(addr));
+		sendto(sock, JJ, sizeof(JJ),0, (struct sockaddr*) &addr, sizeof(addr));
 
 		/* TCPセッションの終了 */
 		close(sock);
 
 
 	}
-
-
-	
-	/* listen するsocketの終了 */
-	close(sock0);
 	
 
 
