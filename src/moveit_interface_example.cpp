be68b3213b7d1c3a4d9c1d0707c1f149b4054760
DONGSIK KIM <51149024+KDS-study@users.noreply.github.com>
2019-11-15 15:32:10 +0900

    Update moveit_interface_example.cpp

--- src/moveit_interface_example.cpp
+++ src/moveit_interface_example.cpp
@@ -93,18 +93,7 @@ int main(int argc, char** argv)
 	int len;
 	int sock;
 
-	/* ソケットの作成 */
-	sock0 = socket(AF_INET, SOCK_STREAM, 0);
-
-	/* ソケットの設定 */
-	addr.sin_family = AF_INET;
-	addr.sin_port = htons(12345);
-	addr.sin_addr.s_addr = INADDR_ANY;
-
-	bind(sock0, (struct sockaddr*) & addr, sizeof(addr));
-
-	/* TCPクライアントからの接続要求を待てる状態にする */
-	listen(sock0, 5);
+	
 
 	char key;
 
@@ -211,17 +200,28 @@ int main(int argc, char** argv)
 		jointout();
 	
 		
-		string JJ = "J" + J1.str();
+		string JJ = "J" + to_string(J1);
 		
+		/* ソケットの作成 */
+		sock0 = socket(AF_INET, SOCK_STREAM, 0);
 
+		/* ソケットの設定 */
+		addr.sin_family = AF_INET;
+		addr.sin_port = htons(12345);
+		addr.sin_addr.s_addr = INADDR_ANY;
+
+		bind(sock0, (struct sockaddr*) &addr, sizeof(addr));
+
+		/* TCPクライアントからの接続要求を待てる状態にする */
+		listen(sock0, 5);
 		
 
 		/* TCPクライアントからの接続要求を受け付ける */
 		len = sizeof(client);
-		sock = accept(sock0, (struct sockaddr *)&client, &len);
+		sock = accept(sock0, (struct sockaddr *) &client, &len);
 
 		/* 送信 */
-		write(sock, JJ, sizeof(JJ));
+		write(sock, JJ, sizeof(JJ),0);
 
 		/* TCPセッションの終了 */
 		close(sock);
